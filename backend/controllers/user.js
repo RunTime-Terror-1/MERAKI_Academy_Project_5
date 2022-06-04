2c71509814c2958041da0d8742b3e79384471f3f
AlhajjajKhalaf <khalaf.sa.alhajjaj@gmail.com>
2022-06-04 09:37:35 +0300

    get Allrestaurants

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,1 +1,21 @@
 const connection = require("../models/db");
+
+
+const getAllRestaurants = (req, res) => {
+    const query = `SELECT * FROM restarents `;
+    connection.query(query, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          massage: "server error",
+          err: err,
+        });
+      }
+    
+      res.status(200).json({
+        success: true,
+        massage: "All the restaurants",
+        result: result,
+      });
+    });
+  };
