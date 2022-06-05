796daaa7cfab3c53b2fb6a025f4b6c93d4ad9fd9
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-06-05 23:57:18 +0300

    nav Edit **

--- frontend/src/components/AdminPanle/SuperAdmin/NavigationMenu/index.js
+++ frontend/src/components/AdminPanle/SuperAdmin/NavigationMenu/index.js
@@ -1,37 +1,51 @@
-import React from "react"
-import "./style.css"
-// BiGitPullRequest
-import {HiUsers} from "react-icons/hi"
-import {BiGitPullRequest} from "react-icons/bi"
+import React from "react";
+import "./style.css";
+import { HiUsers } from "react-icons/hi";
+import { BiGitPullRequest } from "react-icons/bi";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
-export const NavigationBar = (req,res)=>{
+export const NavigationBar = (req, res) => {
+  const userArea = ({
+    name,
+    imgUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Lionel_Messi_20180626.jpg/347px-Lionel_Messi_20180626.jpg",
+  }) => {
+    return (
+      <div>
+        <div>
+          <img src={imgUrl} />
+        </div>
+        <h5>{name} </h5>
+      </div>
+    );
+  };
 
-    const userArea = ({name, imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Lionel_Messi_20180626.jpg/347px-Lionel_Messi_20180626.jpg"})=>{
-        return (
-            <div>
-                <div>
-                    <img src={imgUrl}/>
-                </div>
-                <h5>{name}  </h5>
-            </div>
-        )
-    }
+  const menuButton = ({ text, onClick, icon }) => {
+    return (
+      <div className="Nav-menu-btn">
+        <button onClick={onClick} >
+          {text}
+        </button>
+        {icon}
+      </div>
+    );
+  };
 
-    const menuButton = ({text,onClick ,icon})=>{
-        return(
-            <button onClick={onClick} className="Nav-menu-btn"> 
-            {text}{icon}
-            </button>
-        )
-    }
-
-    return (<div id="nav-menu-panel">
-        <h3>User Information</h3>
-        <div>{userArea({name:"sad"})}</div>
-        <h3>menu</h3>
-        {menuButton({text:"Users" ,icon:<HiUsers/>,onClick:()=>{}})}
-        {menuButton({text:"Requests" ,icon:<BiGitPullRequest/>,onClick:()=>{}})}
-        {menuButton({text:"Create Owner" ,icon:<HiUsers/>,onClick:()=>{}})}
-        
-    </div>)
-}
+  return (
+    <div id="nav-menu-panel">
+      <h3>User Information</h3>
+      <div>{userArea({ name: "sad" })}</div>
+      <h3>menu</h3>
+      {menuButton({ text: "Users", icon: <HiUsers />, onClick: () => {} })}
+      {menuButton({
+        text: "Requests",
+        icon: <BiGitPullRequest />,
+        onClick: () => {},
+      })}
+      {menuButton({
+        text: "Create Owner",
+        icon: <AiOutlineUserAdd />,
+        onClick: () => {},
+      })}
+    </div>
+  );
+};
