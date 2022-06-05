82222855dc6b87a8125f7d9da5de1bac8a35e3d4
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-06-05 13:42:26 +0300

    add to cart and getResturentByname

--- frontend/src/controllers/user.js
+++ frontend/src/controllers/user.js
@@ -3,9 +3,7 @@ const { default: axios } = require("axios");
 class User {
   static async getAllRestaurants({ token }) {
     try {
-      const response = await axios.get(`${process.env.HOSTURL}/user/`, {
-        headers: { authorization: `Bearer ${token}` },
-      });
+      const response = await axios.get(`${process.env.HOSTURL}/user/`);
       return {
         success: true,
         massage: "All the restaurants",
@@ -21,13 +19,9 @@ class User {
   }
 
   static async getRestaurantByName({ token, restaurantName }) {
-    const restaurantName = req.params.name;
     try {
       const response = await axios.get(
-        `${process.env.HOSTURL}/user/${restaurantName}`,
-        {
-          headers: { authorization: `Bearer ${token}` },
-        }
+        `${process.env.HOSTURL}/user/${restaurantName}`
       );
       const result = response.data.result;
       if (result.length) {
@@ -37,21 +31,92 @@ class User {
           result: result,
         };
       } else {
-        return{
+        return {
           success: false,
           massage: `the restaurant with name  ${restaurantName} is not found now `,
         };
       }
-      return {
-        success: true,
-        massage: "All the restaurants",
-        result: response.data.result,
-      };
     } catch (error) {
       return {
         success: false,
         massage: "Server Error",
         error,
+        result: [],
+      };
+    }
+  }
+
+  static async getMealByRestaurant({ restaurant_id }) {
+    try {
+      const response = await axios.get(
+        `${process.env.HOSTURL}/user/${restaurant_id}/meals`
+      );
+      const result = response.data.result;
+      if (result.length) {
+        return {
+          success: true,
+          massage: `The meal ${mealName} is found`,
+          result: result,
+        };
+      } else {
+        return {
+          success: false,
+          massage: "The meals is Not Found",
+        };
+      }
+    } catch (error) {
+      return {
+        success: false,
+        massage: "Server Error",
+        error,
+        result: [],
+      };
+    }
+
+    connection.query(query, data, (err, result) => {
+      if (err) {
+        res.status(500).json({
+          success: false,
+          massage: "Server Error",
+          err: err,
+        });
+      }
+      if (!result.length) {
+        res.status(404).json({
+          success: false,
+          massage: "The meal is Not Found",
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          massage: `The meal ${mealName} is found`,
+          result: result,
+        });
+      }
+    });
+  }
+
+  static async addMealToCart({ quantity, subTotal, meal_id, token }) {
+    try {
+      const body = { quantity, subTotal };
+      const response = await axios.post(
+        `${process.env.HOSTURL}/user/${meal_id}`,
+        body,
+        {
+          headers: { authorization: `Bearer ${token}` },
+        }
+      );
+
+      return {
+        success: true,
+        massage: "add meal to cart",
+        result: response.data.result,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        massage: "Server error",
+        err: error,
       };
     }
   }
