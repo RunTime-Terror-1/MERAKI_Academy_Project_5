1525a7cab1f1bb3aaf608b36635c7409503c1ffc
AlhajjajKhalaf <khalaf.sa.alhajjaj@gmail.com>
2022-06-06 16:01:03 +0300

    Screnn

--- frontend/src/components/Homepage/ScreenHome/index.js
+++ frontend/src/components/Homepage/ScreenHome/index.js
@@ -9,8 +9,17 @@ const ScreenHome = () => {
 
     return (<div className="ScreenHome">
 
-        <div className="SH_first"><div className="SH_first_One" ><input /> <button>let`s go</button></div></div>
-        <div className="SH_second">dddd </div>
+        <div className="SH_first">
+            <div className="SH_first_One" ><input placeholder="Location" /> <button className='SH_FOne_Button'>let`s go</button></div>
+        </div>
+
+
+        <div className="SH_second">
+            <div className='SH_second_One'><img className="SH_secound_imgOne" src="https://img.cdn4dd.com/cdn-cgi/image/fit=contain,width=768,format=auto,quality=50/https://cdn.doordash.com/media/consumer/home/landing/new/ev_fla_wel_alt.jpg"  /></div>
+            <div className="SH_second_Tow"> <h1>Every Flavor Welcome</h1><br/><h2>Best Restaurants in New York City</h2></div>
+            <img src="https://images.deliveryhero.io/image/talabat/restaurants/logo_637776727794264228.jpg?width=115&height=104"/>
+            <img src="https://images.deliveryhero.io/image/talabat/restaurants/Logo_(1)_(7)_637464453581886464.jpg?width=115&height=104"/>
+        </div>
 
 
     </div>
