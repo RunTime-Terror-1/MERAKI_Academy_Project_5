e6aa7464b1bf3ee6096759d1841c4ce22f0fe655
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-06-10 22:10:07 +0300

    working on design super admin panel finish navBar

--- frontend/src/components/AdminPanle/SuperAdmin/NavigationBar/index.js
+++ frontend/src/components/AdminPanle/SuperAdmin/NavigationBar/index.js
@@ -4,9 +4,27 @@ import {AiOutlineMenu } from "react-icons/ai";
 
 //AiOutlineMenu
 export const NavigationBarPanel  = ()=> {
+   const userArea = ({
+        name = "Khaled",
+        imgUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Lionel_Messi_20180626.jpg/347px-Lionel_Messi_20180626.jpg",
+      }) => {
+        return (
+          <div id="user-img2-div">
+            <div>
+              <img src={imgUrl} />
+            </div>
+            <div>
+              <h4>{name} </h4>
+            </div>
+          </div>
+        );
+      };
   return (
     <div id = "NavigationBarPanel">
         <AiOutlineMenu/>
+        <div>
+        {userArea({})}
+        </div>
     </div>
   )
 }
