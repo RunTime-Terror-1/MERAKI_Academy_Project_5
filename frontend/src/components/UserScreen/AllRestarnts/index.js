f9d217e4d58c0e51f7abad1c6cf99efba0bb6612
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-06-17 15:03:45 +0300

    edit all restaurants page

--- frontend/src/components/UserScreen/AllRestarnts/index.js
+++ frontend/src/components/UserScreen/AllRestarnts/index.js
@@ -14,6 +14,10 @@ const AllRestaurants = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageBtn, setPageBtn] = useState([]);
 
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
   const getRestaurants = async () => {
     let responseRest = await User.getAllRestaurants();
     setRestaurants(responseRest.result);
@@ -22,18 +26,14 @@ const AllRestaurants = () => {
     for (let i = 0; i < totalPages; i++) {
       arr.push(i + 1);
     }
-    console.log(arr, totalPages);
     setTotalPages(totalPages);
     setPageBtn(arr);
   };
-
-  useEffect(() => {
-    getRestaurants();
-  }, []);
-
   const buildCategoryIcon = ({ url, text = "Pizza" }) => {
     return (
       <div
+        key={text}
+        id= "categoryIcon"
         onClick={() => {
           navigate("/SortResturants", {
             state: { sortcategory: "pizza" },
@@ -52,6 +52,7 @@ const AllRestaurants = () => {
   const buildRestaurantCard = ({ element, index }) => {
     return (
       <div
+        key={element.id}
         onClick={async () => {
           await localStorage.setItem("restaurantId", element.id);
           dispatch(setrestaurantId({ restId: element.id }));
@@ -59,7 +60,6 @@ const AllRestaurants = () => {
             state: { id: localStorage.getItem("restaurantId") },
           });
         }}
-        key={element.id}
         id="rest-card-div"
       >
         <img src={element.backImg} />
@@ -69,9 +69,8 @@ const AllRestaurants = () => {
   };
   const buildPageButton = (i) => {
     return (
-      <div style={{display:"flex"}}>
+      <div style={{ display: "flex" }} key={i + "k"}>
         <button
-          key={i}
           onClick={() => {
             if (currentPage < i) {
               setCurrentPage(currentPage + 7);
@@ -82,61 +81,66 @@ const AllRestaurants = () => {
         >
           {i}
         </button>
-        {i!==totalPages?<div id="border"></div>:<></>}
+        {i !== totalPages ? <div id="border"></div> : <></>}
       </div>
     );
   };
- 
+  const categories = [
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Zyu-T5WQ7bLxROPZPWo7u65WqNKpnk7NWQ&usqp=CAU",
+      text: "Pizza",
+    },
+    {
+      url: "https://media-cdn.tripadvisor.com/media/photo-s/17/57/7d/17/2-egg-breakfast.jpg",
+      text: "Breakfast",
+    },
+    {
+      url: "https://c8.alamy.com/comp/2F7BRP8/french-potato-pack-box-cartoon-fastfood-fry-potato-isolated-illustration-fast-food-2F7BRP8.jpg",
+      text: "Fast food",
+    },
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLKopRRHoSDAgqTfKGo8tn1y_iggg0CtY-YPVx-5V9elOO0080P-eAJi8zbqtpytywskg&usqp=CAU",
+      text: "Burgess",
+    },
+    {
+      url: "https://thumbs.dreamstime.com/b/vector-illustration-heart-shape-red-fruits-vegetables-healthy-nutrition-organic-concept-flat-style-127159995.jpg",
+      text: "Healthy",
+    },
+  ];
+
   return (
     <div className="AllRestarnts">
       <NavBar />
       <div className="first-restaurant-div">
-        <div className="Continert">
-          <div className="Continer_A">
-            <div className="Continer_A_A">
-              {buildCategoryIcon({
-                url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Zyu-T5WQ7bLxROPZPWo7u65WqNKpnk7NWQ&usqp=CAU",
-                text: "Pizza",
-              })}
-              {buildCategoryIcon({
-                url: "https://media-cdn.tripadvisor.com/media/photo-s/17/57/7d/17/2-egg-breakfast.jpg",
-                text: "Breakfast",
-              })}
-              {buildCategoryIcon({
-                url: "https://c8.alamy.com/comp/2F7BRP8/french-potato-pack-box-cartoon-fastfood-fry-potato-isolated-illustration-fast-food-2F7BRP8.jpg",
-                text: "Fast food",
-              })}
-
-              {buildCategoryIcon({
-                url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLKopRRHoSDAgqTfKGo8tn1y_iggg0CtY-YPVx-5V9elOO0080P-eAJi8zbqtpytywskg&usqp=CAU",
-                text: "Burgess",
-              })}
-
-              {buildCategoryIcon({
-                url: "https://thumbs.dreamstime.com/b/vector-illustration-heart-shape-red-fruits-vegetables-healthy-nutrition-organic-concept-flat-style-127159995.jpg",
-                text: "Healthy",
-              })}
-            </div>
+        <div className="Continert1">
+          <div className="Continer_A_A">
+            {categories.map((category) => {
+              return buildCategoryIcon({
+                url: category.url,
+                text: category.text,
+              });
+            })}
           </div>
-
-          <div id="restaurants-div">
-            {restaurants
-              ? restaurants.map((element, index) => {
-                  if (index >= currentPage - 1 && index < currentPage + 7) {
-                    return buildRestaurantCard({ element, index });
-                  }
-                })
-              : " "}
-          </div>
-          {pageBtn.length > 1 ? (
-            <div id="pagination-div">
-              {pageBtn.map((i) => {
-                return buildPageButton(i);
-              })}
+          <div className="Continert">
+            <div id="restaurants-div">
+              {restaurants
+                ? restaurants.map((element, index) => {
+                    if (index >= currentPage - 1 && index < currentPage + 7) {
+                      return buildRestaurantCard({ element, index });
+                    }
+                  })
+                : " "}
             </div>
-          ) : (
-            <></>
-          )}
+            {pageBtn.length > 1 ? (
+              <div id="pagination-div">
+                {pageBtn.map((i) => {
+                  return buildPageButton(i);
+                })}
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -144,4 +148,3 @@ const AllRestaurants = () => {
 };
 
 export default AllRestaurants;
-
