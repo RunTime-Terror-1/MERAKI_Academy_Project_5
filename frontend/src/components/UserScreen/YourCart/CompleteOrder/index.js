2b4aa2e888039cc00708e1abdd642be7b6eda992
HussamAlkhalaileh <alkhalaileh.hussam@gmail.com>
2022-06-18 21:13:34 +0300

    readme

--- frontend/src/components/UserScreen/YourCart/CompleteOrder/index.js
+++ frontend/src/components/UserScreen/YourCart/CompleteOrder/index.js
@@ -492,7 +492,7 @@ const CompleteOrder = () => {
               
                     </div>
                   </div>
-                </div>
+                
               ) : (
                 ""
               )}
