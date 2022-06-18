2dfcd47ece9d47603faeab4dce172186a2e5fa34
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-06-19 02:38:29 +0300

    Finally

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,8 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 
-// export const hostUrl = "https://meraki-project5.herokuapp.com";
-export const hostUrl = "http://localhost:5000";
+export const hostUrl = "https://meraki-project5.herokuapp.com";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
